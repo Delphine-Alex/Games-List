23bf3975116538489adcf8084f8e197cfcc70299
Delphine <delphine.pva@gmail.com>
2020-12-16 19:05:11 +0100

    Add CSS on GameList and Game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,20 +1,22 @@
 import React, {useState} from 'react';
 
-import "./Game.css"
+import "./Game.css";
 
 const Game = ({ name, background_image, rating,released}) => {
     const [favorite, setFavorite] = useState(false);
 
         return(
-            <div>
-                <img className="img" src={background_image} alt={name} />
-                <p>{name} {favorite && <img className="favorite" src="https://upload.wikimedia.org/wikipedia/commons/3/35/Emoji_u2665.svg" alt="Like"/>}</p>
-                <p>Rating : {rating}</p>
-                <p>Released : {released}</p>
-                <div>
-                    <button className="favorite-btn" onClick={() => setFavorite(!favorite)}>
-                    {favorite ? "Remove from favorites" : "Add to favorites"}
-                    </button>
+            <div className="card">
+                <img className="card-img" src={background_image} alt={name} />
+                <div className="card-body">
+                    <h2>{name} {favorite && <img className="favorite" src="https://upload.wikimedia.org/wikipedia/commons/3/35/Emoji_u2665.svg" alt="Like"/>}</h2>
+                    <p><strong>Rating :</strong> {rating}/5</p>
+                    <p><strong>Released :</strong> {released}</p>
+                    <div>
+                        <button className="favorite-btn" onClick={() => setFavorite(!favorite)}>
+                        {favorite ? "Remove from favorites" : "Add to favorites"}
+                        </button>
+                    </div>
                 </div>
             </div>
         )
